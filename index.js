069c6cb270c884cfc6edeca8b820ac8edce492c7
Alejandro Vergara Tamayo <73759053+Alejotamayo28@users.noreply.github.com>
2023-02-27 17:56:02 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+import * as entrypoint from "./controller.js";
+import chalk from 'chalk'
+
+console.log(entrypoint.multiplicacion(10, 2))
+console.log(entrypoint.suma(10, 2))
+console.log(chalk.blue(entrypoint.multiplicacion(10,2)))
